5b881256f5c2938bae30d5c9e24477563525e5bd
ckr8305 <ckr8305@naver.com>
2025-05-16 16:26:53 +0900

    chore(#8): 컬럼명 추가, 주석 삭제

--- src/main/java/org/example/stellog/starbucks/domain/retpository/StarbucksRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.stellog.starbucks.domain.retpository;
-
-import org.example.stellog.starbucks.domain.Starbucks;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StarbucksRepository extends JpaRepository<Starbucks, Long> {
-}
