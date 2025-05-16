88fd36c10fd09e9746462b79e1a2edec6cf082fd
ckr8305 <ckr8305@naver.com>
2025-05-16 16:54:00 +0900

    feature(#8): 방과 스타벅스 별 립 목록 조회 기능 추가

--- src/main/java/org/example/stellog/review/domain/repository/StarbucksReviewRepository.java
+++ src/main/java/org/example/stellog/review/domain/repository/StarbucksReviewRepository.java
@@ -2,8 +2,11 @@ package org.example.stellog.review.domain.repository;
 
 import org.example.stellog.review.domain.Review;
 import org.example.stellog.review.domain.StarbucksReview;
+import org.example.stellog.room.domain.Room;
 import org.example.stellog.starbucks.domain.Starbucks;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,4 +17,8 @@ public interface StarbucksReviewRepository extends JpaRepository<StarbucksReview
     Optional<StarbucksReview> findByReview(Review review);
 
     List<StarbucksReview> findAllByReviewIn(List<Review> reviews);
+
+    @Query("SELECT sr FROM StarbucksReview sr WHERE sr.starbucks = :starbucks AND sr.review.room = :room")
+    List<StarbucksReview> findAllByStarbucksAndRoom(@Param("starbucks") Starbucks starbucks, @Param("room") Room room);
+
 }
