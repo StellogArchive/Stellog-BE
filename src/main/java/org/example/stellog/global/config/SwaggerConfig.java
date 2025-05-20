4806cb73c7390b0017560242f951c4619fe07930
ckr8305 <ckr8305@naver.com>
2025-05-20 15:58:23 +0900

    refactor(#8): 스웨거 Authorize 추가

--- src/main/java/org/example/stellog/global/config/SwaggerConfig.java
+++ src/main/java/org/example/stellog/global/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package org.example.stellog.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -21,11 +23,26 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        // Security Scheme 정의
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        // Security Requirement 정의
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("BearerAuth");
+
         return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo())
-                .servers(servers());
+                .info(new Info().title("stellog API")
+                        .description("")
+                        .version("1.0.0"))
+                .servers(servers())
+                .addSecurityItem(securityRequirement)
+                .components(new Components().addSecuritySchemes("BearerAuth", securityScheme));
     }
+
 
     private Info apiInfo() {
         return new Info()
