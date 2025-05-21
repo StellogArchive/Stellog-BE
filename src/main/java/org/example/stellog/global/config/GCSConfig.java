94739301e421c16fbd2a680aab7f2ecfe35cd5fe
ckr8305 <ckr8305@naver.com>
2025-05-21 09:52:55 +0900

    fix(#10): GCP credentials 경로 문제 해결을 위해 classpath 리소스로 변경

--- src/main/java/org/example/stellog/global/config/GCSConfig.java
+++ src/main/java/org/example/stellog/global/config/GCSConfig.java
@@ -3,24 +3,32 @@ package org.example.stellog.global.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
+import org.example.stellog.global.gcs.exception.GCSFileNotFoundException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 @Component
 public class GCSConfig {
+
     @Value("${spring.cloud.gcp.storage.credentials.path}")
     private String credentialJson;
 
     @Bean
     public Storage storage() throws IOException {
-        FileInputStream serviceAccountStream = new FileInputStream(credentialJson);
+        InputStream serviceAccountStream = getClass().getClassLoader().getResourceAsStream(credentialJson);
+        if (serviceAccountStream == null) {
+            throw new GCSFileNotFoundException("GCP credentials 파일이 classpath에 존재하지 않습니다: " + credentialJson);
+        }
         GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccountStream);
-        return StorageOptions.newBuilder().setCredentials(credentials).build().getService();
+        return StorageOptions.newBuilder()
+                .setCredentials(credentials)
+                .build()
+                .getService();
     }
 }
