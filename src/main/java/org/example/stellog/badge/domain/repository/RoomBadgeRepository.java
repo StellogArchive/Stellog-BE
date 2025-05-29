84f7a31cad7b70c343e8e79e260c74e2f6ad4bb0
ckr8305 <ckr8305@naver.com>
2025-05-29 10:29:07 +0900

    feat(#18): 방 상세 조회에 배지 리스트 반환

--- src/main/java/org/example/stellog/badge/domain/repository/RoomBadgeRepository.java
+++ src/main/java/org/example/stellog/badge/domain/repository/RoomBadgeRepository.java
@@ -4,6 +4,11 @@ import org.example.stellog.badge.domain.RoomBadge;
 import org.example.stellog.room.domain.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RoomBadgeRepository extends JpaRepository<RoomBadge, Long> {
     boolean existsByRoomAndBadgeId(Room room, Long badgeId);
+
+    List<RoomBadge> findAllByRoom(Room room);
+
 }
