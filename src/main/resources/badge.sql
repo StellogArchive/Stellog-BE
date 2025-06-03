941bd9ef77ed3bb87a9273a463a3c5a06c2d77ff
ckr8305 <ckr8305@naver.com>
2025-06-03 15:19:55 +0900

    feat(#18): 방 삭제할 때 리뷰 삭제

---
+++ src/main/resources/badge.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS badge
+(
+    badge_id    INT PRIMARY KEY,
+    name        varchar(100)
+    );
+
+INSERT IGNORE INTO badge (badge_id, name)
+VALUES (1, '스타벅스 첫 방문'),
+       (2, '스타벅스 5곳 방문'),
+       (3, '스타벅스 10곳 방문'),
+       (4, '스타벅스 20곳 방문'),
+       (5, '스타벅스 전체 방문'),
+       (6, '인기있는 동선에 선정');
